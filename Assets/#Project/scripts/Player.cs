e0a2d78e951f9d975b1632b0a463edfbfb036d54
ZAJKO Patryk <STG2283@BRUFOR.LAN>
2024-04-26 15:59:26 +0200

    personnagePrincipalFini

---
+++ Assets/#Project/scripts/Player.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using Unity.VisualScripting;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    float moveSpeed = 15;
+    float mMoveSpeed = 5;
+    float horizontalInput;
+    float verticalInput;
+    Rigidbody m_RigidBody;
+    float cd=2f;
+    float time;
+    [SerializeField] Transform fleche;
+    Transform fleche2;
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+        time=0;
+    }
+
+    void Update()
+    {
+        Vector3 mouseMovement;
+        mouseMovement = mMoveSpeed * new Vector3(-Input.GetAxis("Mouse Y"),Input.GetAxis("Mouse X"),0);
+        horizontalInput = getXaxis();
+        verticalInput = getYaxis();
+        Vector3 m_Input = new Vector3(horizontalInput,0,verticalInput)* Time.deltaTime * moveSpeed;
+        m_Input = transform.TransformDirection(m_Input);
+        Vector3 newPoss = transform.position + m_Input;
+        m_RigidBody.MovePosition(newPoss);
+        transform.eulerAngles += mouseMovement;
+        transform.InverseTransformDirection(mouseMovement);
+        if(time<cd)
+        {
+        time = time + Time.deltaTime;
+        }
+        else
+        {
+            showBullet();
+            if(Input.GetButton("Fire1"))
+        {
+           shoot();
+           fleche.transform.parent = null;
+           fleche = Instantiate(fleche,fleche.transform.position,fleche.rotation);
+           fleche.parent = gameObject.GetComponent<Transform>();
+        }
+        }
+        
+    }
+
+
+
+    float getXaxis()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+
+    float getYaxis()
+    {
+        return Input.GetAxis("Vertical");
+    }
+
+
+    private void shoot()
+    {
+        
+        if(time > cd)
+        {
+            time=0;
+            fleche.GetComponent<Shoted>().Shot();
+        }
+    }
+
+    private void showBullet()
+    {
+        fleche.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/#Project/scripts/Shoted.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoted : MonoBehaviour
+{
+    Boolean shot;
+    Rigidbody m_RigidBody;
+    float mMoveSpeed = 20;
+    Vector3 newPoss;
+    float cd=0.1f;
+    float time;
+
+    Boolean activateCollision = false;
+    //float moveSpeed = 15;
+    //float mMoveSpeed = 5;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        shot = false;
+        m_RigidBody = GetComponent<Rigidbody>();
+        gameObject.SetActive(false);
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(shot)
+        {
+        
+        newPoss = transform.position + transform.forward * mMoveSpeed * Time.deltaTime;
+        transform.InverseTransformDirection(newPoss);
+        m_RigidBody.MovePosition(newPoss);
+        if(time<cd)
+        {
+        time = time + Time.deltaTime;
+        }
+        else
+        {if(activateCollision == false)
+        {
+            activateCollision = true;
+        }
+        }
+        }
+        }
+    public void Shot()
+    {
+        shot = true;
+    }
+
+   private void OnTriggerEnter(Collider other) {
+    if(activateCollision)
+    {
+        Destroy(gameObject);
+    }
+   }
+       
+    }
+    
